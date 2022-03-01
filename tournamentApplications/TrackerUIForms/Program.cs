e63b33dc9b88eab58f2ea356b3e2914bf99cfc08
Dario Van Hasselt <dariovanhasselt@hotmail.be>
2022-03-01 16:09:43 +0100

    Merge branch 'master' of https://github.com/xerio1001/C-Sharp

--- tournamentApplications/TrackerUIForms/Program.cs
+++ tournamentApplications/TrackerUIForms/Program.cs
@@ -19,15 +19,11 @@ namespace TrackerUIForms
             Application.SetCompatibleTextRenderingDefault(false);
 
             ApplicationConfiguration.Initialize();
-<<<<<<< HEAD
-            Application.Run(new TournamentDashboardForm());
-=======
-            // initialize the database connections.
-            TrackerLibrary.GlobalConfig.InitializeConnections(DatabaseType.Sql);
             Application.Run(new CreateTeamForm());
 
+            // initialize the database connections.
+            TrackerLibrary.GlobalConfig.InitializeConnections(DatabaseType.Sql);
             //Application.Run(new TournamentDashboardForm());
->>>>>>> f542e07445d998b4b9bd6ff1d9a3f5fe6b6eed27
         }
     }
 }
